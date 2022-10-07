12475fcc039fd057a0164c192bf1de50dd94193a
salsabila juandira <92494551+sjuandira@users.noreply.github.com>
2022-10-07 19:56:47 +0700

    Update Praktikum7.java

--- Praktikum7.java
+++ Praktikum7.java
@@ -2,10 +2,6 @@
 package praktikum7;
 
 public class Praktikum7 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Mahasiswa7 mhs= new Mahasiswa7("Salsabila","2117051012",88,90,88);
         mhs.printData("Ilmu Komputer");
