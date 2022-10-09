04d4c13e360b97eaa961518f3864986315b1c3f4
salsabila juandira <92494551+sjuandira@users.noreply.github.com>
2022-10-09 17:53:20 +0700

    Update method.java

--- method.java
+++ method.java
@@ -1,1 +1,12 @@
 
+package praktikum7;
+
+public class Praktikum7 {
+    public static void main(String[] args) {
+        Mahasiswa7 mhs= new Mahasiswa7("Salsabila","2117051012",88,90,88);
+        mhs.printData("Ilmu Komputer");
+        System.out.println("Nilai akhir anda: "+mhs.nilaiAkhir());
+       
+    }
+    
+}
